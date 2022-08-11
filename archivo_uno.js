e07af093369756e6671e91321484ffe895455473
becca221b <86609729+becca221b@users.noreply.github.com>
2022-08-10 21:59:01 -0300

    Update archivo_uno.js
    Commit 2

--- archivo_uno.js
+++ archivo_uno.js
@@ -13,3 +13,18 @@ y devolverle la suma de dichos numeros
 const firstNumber = parseInt(prompt("Ingresa un numero"));
 const secondNumber = parseInt(prompt("Ingresa otro numero"));
 alert( `La suma de ${firstNumber} y ${secondNumber} es ${firstNumber + secondNumber}`);
+
+/*3) Pedirle a un usuario que ingrese dos numeros
+y devolverle el doble de la suma de dichos numeros
+*/
+const numeroUno= parseInt(prompt("Ingrese un número"))
+const numeroDos=parseInt(prompt("Ingrese otro número"))
+alert(`El doble de la suma de ${numeroUno} + ${numeroDos} es ${((numeroUno+numeroDos)*2)}`)
+
+/*
+4) Pedirle al usuario que ingrese el ancho y el alto 
+de una habitacion y calcular la superficie.
+*/
+const alto= parseInt(prompt("Ingrese la medida del alto"))
+const ancho= parseInt(prompt("Ingrese la medida del ancho"))
+alert(`La superficie es ${alto*ancho}`)
