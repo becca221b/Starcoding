f919355f91ed9cc334a69152834b8e11de5ea2a0
becca221b <chake_rebeca@hotmail.com>
2022-08-15 20:29:30 -0300

    carpeta Intro

---
+++ Introducción/archivo_uno.js
@@ -0,0 +1,60 @@
+/*
+EJERCICIOS 
+1) Pedir al usuario que ingrese su nombre. Guardar
+ese nombre en una variable y utilizarlo para saludar al 
+usuario (por ejemplo: "Hola Alexis").
+*/
+//const nombre= prompt("Ingresa tu nombre")
+//alert("Hola "+nombre)
+
+/*2) Pedirle a un usuario que ingrese dos numeros
+y devolverle la suma de dichos numeros
+*/
+//const firstNumber = parseInt(prompt("Ingresa un numero"));
+//const secondNumber = parseInt(prompt("Ingresa otro numero"));
+//alert( `La suma de ${firstNumber} y ${secondNumber} es ${firstNumber + secondNumber}`);
+
+/*3) Pedirle a un usuario que ingrese dos numeros
+y devolverle el doble de la suma de dichos numeros
+*/
+//const numeroUno= parseInt(prompt("Ingrese un número"))
+//const numeroDos=parseInt(prompt("Ingrese otro número"))
+//alert(`El doble de la suma de ${numeroUno} + ${numeroDos} es ${((numeroUno+numeroDos)*2)}`)
+
+/*
+4) Pedirle al usuario que ingrese el ancho y el alto 
+de una habitacion y calcular la superficie.
+*/
+//const alto= parseInt(prompt("Ingrese la medida del alto"))
+//const ancho= parseInt(prompt("Ingrese la medida del ancho"))
+//alert(`La superficie es ${alto*ancho}`)
+
+/*
+5) Pedirle al usuario que ingrese su nombre y devolverle
+el nombre todo en minusculas
+*/
+//const nombre=prompt("Ingrese su nombre")
+//alert(nombre.toLowerCase())
+
+/*
+6) Pedirle al usuario que ingrese su apellido y devolverle
+el apellido todo en mayuscula.
+*/
+/*
+6) Pedirle al usuario que ingrese su apellido y devolverle
+el apellido todo en mayuscula.
+*/
+//const apellido= prompt("Ingrese su apellido")
+//alert(`Tu apellido en minúsculas es `+ apellido.toUpperCase())
+
+/*
+7) Pedirle al usuario el nombre, el apellido y la 
+edad. Mostrar el siguiente mensaje:
+Nombre: Juan
+Apellido: Perez
+Edad: 20
+*/
+//const nombre=prompt("Ingrese su nombre")
+//const apellido=prompt("Ingrese su apellido")
+//const edad=prompt("Ingrese su edad")
+//alert("Nombre: "+nombre+"\nApellido: "+apellido+"\nEdad: "+edad)
