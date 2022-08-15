a3d35e0aa99d0ca5698c693e743e1cde67e4835a
becca221b <chake_rebeca@hotmail.com>
2022-08-15 20:42:45 -0300

    update html y js files

--- Array/script.js
+++ Array/script.js
@@ -1,1 +1,34 @@
+//EJERCICIOS
 
+/*
+Dada la siguiente lista de invitados en ese orden:
+Lucas,
+Matias,
+Jose,
+Pedro,
+Martina, 
+Marcelo, 
+Esteban, 
+Marcela, 
+Martin
+ 1) Informar por consola el numero de invitados 
+ */
+/*2) Imprimir por consola el nombre del ultimo invitado
+ */
+/*3) A ultimo momento, se invito a Pedro a la fiesta, por lo que
+ hay que agregarlo al final de la lista.
+ */
+/*4) Media hora antes de empezar Marcela confirmo que no podia asistir.
+ Generar una nueva lista que contenga los invitados que confirmaron su
+ presencia.
+ */
+/*5) Suponiendo que los invitados consumieron la siguente cantidad de platos
+    Lucas: 3 platos
+    Esteban: 1 platos
+    Jose: 1 plato
+    Los restantes: 4 platos
+Calular el numero total de platos consumidos al final de la fiesta
+*/
+/*
+6) Acomodar los invitados por orden alfabetico.
+*/
