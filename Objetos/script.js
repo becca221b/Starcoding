9bb964f1d694cd18800f335d963d02b6e2b8373c
becca221b <chake_rebeca@hotmail.com>
2022-08-15 22:26:05 -0300

    lista de ejercicios

--- Objetos/script.js
+++ Objetos/script.js
@@ -1,1 +1,36 @@
+//EJERCICIOS
 
+/*
+1) Crear una clase Deportes, que reciba el atributo nombre
+y un metodo que permita mostrar por consola ese nombre.
+Ademas, crear un metodo que permita mostrar si el deporte es
+individual o por equipos.
+Crear una subclase que sea DeportesDeEquipo que permita ademas del nombre, 
+el numero de integrantes que deben formar parte de ese equipo. 
+Crear un metodo para
+mostrar el siguiente mensaje: 
+"Para jugar al futbol necesitas 11 jugadores por equipo".
+ Tener en cuenta que 'futbol' y 11 son valores que 
+ pueden variar dependiedo
+ del deporte (por ejemplo, si fuese basket, seria 5 jugadores)
+ Futbol
+ Basket
+ Tenis
+PARA ESTA ACTIVIDAD, USAR LO VISTO RESPECTO DE CLASES Y OBJETOS
+*/
+
+/*2) Andrea y Martin deciden hacer un picnic al aire libre.
+Andrea propone llevar una bolsa con 2 cervezas, 2 sandwiches y 1 bolsa
+de papas fritas. Por su parte, Martin lleva 1 porcion de torta, una gaseosa
+y 1 paquete de galletitas dulces.
+CREAR UN OBJETO QUE REPRESENTE CADA UNA DE LAS BOLSAS,
+DONDE CADA PRODUCTO TENGA LA CANTIDAD QUE LLEVO CADA UNO.
+REALIZAR LAS SIGUIENTES TAREAS:
+a) Mostrar por consola los productos que llevo cada uno
+(solo el nombre del producto)
+b) En el camino al picnic, la bolsa de Andrea se rompio, 
+por lo que tuvieron que poner todo en la bolsa de Martin.
+Realizar el cambio correspondiente para que la bolsa de Martin
+tenga el total de los productos del picnic.
+c) Calcular el total de productos que llevaron al picnic
+*/
