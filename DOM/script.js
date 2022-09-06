527a61cea2e7db70f36a2b5af5474861036a1555
becca221b <86609729+becca221b@users.noreply.github.com>
2022-09-06 20:37:55 -0300

    Manipulando nodos

--- DOM/script.js
+++ DOM/script.js
@@ -1,6 +1,7 @@
 //DOM Docuemnt Object Model
 //Acceder a un nodo
 //1) Por el tag name
+/*
 const header= document.getElementsByTagName("header")
 console.log(header)
 //2)Por la clase
@@ -22,3 +23,20 @@ console.log(repos)
 //5)querySelectorAll
 const usernames= document.querySelectorAll(".username")
 console.log(usernames)
+*/
+//Acceder y modificar texto
+const username= document.querySelector(".username")
+//username.textContent="Alexis"
+username.innerText="Rebeca"
+
+//Acceder y modificar src de imagen
+const avatar= document.querySelector("#avatar")
+avatar.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrCXYdTj9qW1YHfbtaW6Ea_5U3fg89jGnRSw&usqp=CAU"
+//console.log(avatar)
+
+//Incorporar html dentro de JS
+const list= document.querySelector("#repos-list")
+list.innerHTML= '<li>Un Repo</li> <br> <li>Otro Repo</li>'
+
+//console.log(list.outerHTML)
+//console.log(list.innerHTML)
