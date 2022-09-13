566bf67d9daa73dc148adf1fe01a146c9d0c583a
becca221b <86609729+becca221b@users.noreply.github.com>
2022-09-12 21:52:59 -0300

    update script.js

--- Events/script.js
+++ Events/script.js
@@ -1,1 +1,22 @@
+const logo= document.querySelector(".puzzle-logo")
+const sayHello= ()=> {
+	alert("Hola Mundo")
+  logo.removeEventListener('click',sayHello)
+}
+logo.addEventListener('click',sayHello)
+//boton.onclick= sayHello= ()=> alert("Hola Mundo")
+
+const colorSelected= ()=>{
+	console.log("Color Selected")
+}
+
+const squareColors= document.querySelectorAll(".color")
+squareColors.forEach(cuadricula=>cuadricula.addEventListener('click',colorSelected))
+
+/*
+const cuadricula= document.querySelector(".red")
+const changeColor= ()=>{
+	const tabla= document.querySelector("table")
+	tabla.classList.add("red")
+}*/
 
