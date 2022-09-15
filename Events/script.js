7db00e8d2b48d2bd56938a96d7d0a6dac039aca6
becca221b <86609729+becca221b@users.noreply.github.com>
2022-09-15 20:58:33 -0300

    paint little squares

--- Events/script.js
+++ Events/script.js
@@ -6,25 +6,25 @@ const sayHello= ()=> {
 logo.addEventListener('click',sayHello)
 //boton.onclick= sayHello= ()=> alert("Hola Mundo")
 
-const colorSelected= ()=>{
+let color;
 
-	console.log("Color Selected")
-}
-/*
-const cuadricula= document.querySelector(".red")
-const changeColor= ()=>{
-	const tabla= document.querySelector("table")
-	tabla.classList.add("red")
-}*/
 const changeColor=(e)=>{
-	const color= e.target.id
+	color= e.target.id
   console.log(color)
-  const tabla= document.querySelector("table")
-	tabla.classList.add(color)
-  //tabla.removeEventListener('click',changeColor)
+  const bloque= document.querySelector(".painterBlock")
+	bloque.classList.add(color)
 }
 
+const paintSquare=(e)=>{
+	const selectedBlock=e.target.id
+  const numDeBloque= document.getElementById(selectedBlock)
+ 	numDeBloque.classList.add(color)
+  //console.log(numDeBloque)
+}
 
 const squareColors= document.querySelectorAll(".color")
 squareColors.forEach(cuadricula=>cuadricula.addEventListener('click',changeColor))
+
+const blocks= document.querySelectorAll(".painterBlock")
+blocks.forEach(cuadritos=>cuadritos.addEventListener('click',paintSquare))
 
