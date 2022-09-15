c00198d2cb1e520c45de209f3e80a49e1cbb02e0
becca221b <86609729+becca221b@users.noreply.github.com>
2022-09-14 21:30:04 -0300

    pintar tabla

--- Events/script.js
+++ Events/script.js
@@ -7,16 +7,24 @@ logo.addEventListener('click',sayHello)
 //boton.onclick= sayHello= ()=> alert("Hola Mundo")
 
 const colorSelected= ()=>{
+
 	console.log("Color Selected")
 }
-
-const squareColors= document.querySelectorAll(".color")
-squareColors.forEach(cuadricula=>cuadricula.addEventListener('click',colorSelected))
-
 /*
 const cuadricula= document.querySelector(".red")
 const changeColor= ()=>{
 	const tabla= document.querySelector("table")
 	tabla.classList.add("red")
 }*/
+const changeColor=(e)=>{
+	const color= e.target.id
+  console.log(color)
+  const tabla= document.querySelector("table")
+	tabla.classList.add(color)
+  //tabla.removeEventListener('click',changeColor)
+}
+
+
+const squareColors= document.querySelectorAll(".color")
+squareColors.forEach(cuadricula=>cuadricula.addEventListener('click',changeColor))
 
