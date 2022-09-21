2cfc86bb469ed925aa0efce773d9b5449677d7b1
becca221b <86609729+becca221b@users.noreply.github.com>
2022-09-21 16:49:48 -0300

    Destructuring

--- Extras/script.js
+++ Extras/script.js
@@ -1,1 +1,34 @@
+//Destructuring
+const names= ["Andres","Julio","Federico"]
+
+const person1={
+	firstName:"Julio",
+	job: "Secretario",
+  city: "Corrientes"
+}
+//ARRAYS
+//Sin destructuring
+/*
+const user1= names[0]
+const user2= names[1]
+const user3= names[2]
+
+console.log(user1,user2,user3)
+*/
+//Con destructuring
+/*
+const [user1,user2]=names
+console.log(user1,user2)
+*/
+//OBJETOS
+//Sin destructuring
+/*
+const user1= person1.firstName
+console.log(user1)
+const job=person1.job
+console.log(job)
+*/
+//Con destructuring
+const {firstName:user1,job}= person1
+console.log(user1, job)
 
