d980dff910bb6ce83c4a09f2429441fa0995800f
becca221b <86609729+becca221b@users.noreply.github.com>
2022-09-21 21:48:36 -0300

    En funciones

--- Extras/script.js
+++ Extras/script.js
@@ -29,6 +29,28 @@ const job=person1.job
 console.log(job)
 */
 //Con destructuring
-const {firstName:user1,job}= person1
-console.log(user1, job)
+//const {firstName:user1,job}= person1
+//console.log(user1, job)
+
+//SPREAD OPERATOR (...)
+//1) Para concatenar arrays (concat)
+/*
+const mergedArray= [...names,...names2]
+console.log(mergedArray)
+*/
+//2) Para copiar un array
+/*
+const copiedArray= [...names]
+console.log(copiedArray)
+*/
+//3) Obtener elementos del array
+/*
+const [user1,...groupedNames]= names
+console.log(user1)
+console.log(groupedNames)
+console.log(names)
+*/
+//En Funciones
+const logValues=(value)=>(console.log(value))
+logValues("hola")
 
